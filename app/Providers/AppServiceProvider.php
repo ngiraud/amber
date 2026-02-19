1ec696c2b407656c177f6eae1e492c6b785da32c
Nicolas Giraud <contact@ngiraud.me>
2026-02-19 10:44:46 +0100

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configurePasswords();
+        $this->configureRelations();
+        $this->configureResources();
+        $this->configureUrl();
+        $this->configureVite();
+    }
+
+    protected function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            ! $this->app->environment(['local', 'testing', 'staging'])
+        );
+    }
+
+    protected function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    protected function configureModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    protected function configurePasswords(): void
+    {
+        Password::defaults(fn (): ?Password => app()->isProduction()
+            ? Password::min(12)
+                ->mixedCase()
+                ->letters()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()
+            : null
+        );
+    }
+
+    protected function configureResources(): void
+    {
+        JsonResource::withoutWrapping();
+    }
+
+    protected function configureUrl(): void
+    {
+        if (! $this->app->environment('local')) {
+            URL::forceScheme('https');
+        }
+    }
+
+    protected function configureVite(): void
+    {
+        Vite::prefetch(concurrency: 3);
+    }
+
+    protected function configureRelations(): void
+    {
+        //        Relation::enforceMorphMap([
+        //            1 => Post::class,
+        //            2 => Comment::class,
+        //        ]);
+    }
+}
