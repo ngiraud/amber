9e142db3ec3f3e8257069e9dad09ee5ac63bb1ed
Nicolas Giraud <contact@ngiraud.me>
2026-02-23 18:40:45 +0100

    Update RecordActivityEvent.php

--- app/Actions/Activity/RecordActivityEvent.php
+++ app/Actions/Activity/RecordActivityEvent.php
@@ -14,18 +14,15 @@ class RecordActivityEvent extends Action
 {
     public function handle(ActivityEventData $data, ?Session $activeSession = null): ?ActivityEvent
     {
-        $projectRepositoryId = $data->projectRepository->id;
-        $projectId = $data->projectRepository->project_id;
-
         // Not sure about this yet, because we record an event each time a file has been "watched" if activated
         //        $activeSession ??= Session::findActive();
 
-        $sessionId = ($activeSession?->project_id === $projectId) ? $activeSession->id : null;
+        $sessionId = ($activeSession?->project_id === $data->projectRepository->project_id) ? $activeSession->id : null;
 
         $event = ActivityEvent::firstOrCreate(
             [
-                'project_id' => $projectId,
-                'project_repository_id' => $projectRepositoryId,
+                'project_id' => $data->projectRepository->project_id,
+                'project_repository_id' => $data->projectRepository->id,
                 'source_type' => $data->sourceType,
                 'type' => $data->type,
                 'occurred_at' => $data->occurredAt,
