8b78eefb1d39d48bd80002fc10659f97b3e22144
Nicolas Giraud <contact@ngiraud.me>
2026-02-24 12:23:57 +0100

    wip

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -6,6 +6,6 @@ import { TimerResetIcon } from 'lucide-vue-next';
     <div
         class="flex size-full items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
     >
-        <TimerResetIcon class="size-6 stroke-current text-white dark:text-black"/>
+        <TimerResetIcon class="stroke-current text-white dark:text-black"/>
     </div>
 </template>
