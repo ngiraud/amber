51eff335860217503903b4f506432e47c6d8e635
Nicolas Giraud <contact@ngiraud.me>
2026-02-24 12:23:00 +0100

    wip

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { TimerResetIcon } from 'lucide-vue-next';
+</script>
+
+<template>
+    <div
+        class="flex size-full items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
+    >
+        <TimerResetIcon class="size-6 stroke-current text-white dark:text-black"/>
+    </div>
+</template>
