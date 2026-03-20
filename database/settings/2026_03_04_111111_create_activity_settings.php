f67e895cbb7a0fb0a169ab28ae0a273f9ef9e356
Nicolas Giraud <contact@ngiraud.me>
2026-03-20 10:32:40 +0100

    chore: update .env.example and defaults for activity settings

--- database/settings/2026_03_04_111111_create_activity_settings.php
+++ database/settings/2026_03_04_111111_create_activity_settings.php
@@ -8,7 +8,7 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('activity.idle_timeout_minutes', 20);
+        $this->migrator->add('activity.idle_timeout_minutes', 30);
         $this->migrator->add('activity.block_end_padding_minutes', 5);
         $this->migrator->add('activity.manual_session_reminder_minutes', 60);
         $this->migrator->add('activity.last_scan_completed_at', null);
